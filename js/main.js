65f1381204aae122d1fcc7699fe00608c457a909
Ysanryo <167697367+Ysanryo@users.noreply.github.com>
2024-05-06 20:34:47 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-window.onload = function(){ 
-$('a[href^=#]').click(function() {
-var speed = 500;
-var href = $(this).attr("href");
-var target = $(href == "#" || href == "" ? 'html' : href);
-var position = target.offset().top;
-$('html').animate({scrollTop:position}, speed, 'swing');
-return false;
+$(document).ready(function() {
+  $('a[href^="#"]').click(function() {
+    var speed = 500;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('html, body').animate({scrollTop: position}, speed, 'swing');
+    return false;
+  });
 });
-};
